752b3a7b8a9147b3858a61b4afc4233754c30a07
Andres Cortes <tax.cortes@gmail.com>
2023-11-01 21:28:28 -0500

    cambios y anexos para inicios de´sesión

--- Express/Enrutamiento/app.js
+++ Express/Enrutamiento/app.js
@@ -3,9 +3,11 @@ const express = require('express')
 // con este determinamos que usaremos un app con express
 const app = express()
 
+//Permite obtener la información enviada por medio de POST
+app.use(express.urlencoded({extends:true}))
 
 //Enrutamiento de nuestra pagina
-
+/*
 app.get('/',(req,res)=>{
 res.sendFile(__dirname +'/index.html')
 })
@@ -28,6 +30,30 @@ res.send('Modulo inicio sesion')
 app.get('*',(req,res)=>{
 res.send('Pagina no encontrada')
 })
+app.listen(3000,()=>{
+    console.log(`Se inicio la app en HTTP://localhost:3000`)
+})
+*/
+
+// crear un archivo de inicio de sesion de html que contenga un formulario de inicio sesión
+
+/*app.get('/',(req,res) => {
+    res.send('Esta es la pagina de inicio')
+    })*/
+app.get ('/Index',(req,res) => {
+    res.sendFile(__dirname + '/inicioSesion.html')
+})
+
+app.post('/Bienvenido',(req,res) =>{
+    const nombre = req.body.user
+    const contra = req.body.pass
+    res.send(`Gracias por iniciar sesion ${nombre}`)
+})
+
+app.get ('*',(req,res) =>{
+    res.sendFile(__dirname +'/error.html')
+})
+
 app.listen(3000,()=>{
     console.log(`Se inicio la app en HTTP://localhost:3000`)
 })
